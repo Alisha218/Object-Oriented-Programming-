eed309cd3d56d29d3d59218b37e6e2ba3a6c4146
Alisha218 <bscs2312105@szabist.pk>
2025-09-25 21:08:20 +0500

    Add files via upload

---
+++ SummerWork.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.summerwork;
+
+/**
+ *
+ * @author Alisha
+ */
+public class SummerWork {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
